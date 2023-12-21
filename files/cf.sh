1e85ce68a870140120aaeea7c9e2d73dc83d778d
sanzVPN <154396194+sanzVPN@users.noreply.github.com>
2023-12-21 17:31:34 +0700

    Update cf.sh

--- files/cf.sh
+++ files/cf.sh
@@ -1,19 +1,26 @@
-#!/bin/bash
+#!/bin/bash#!/bin/bash
 MYIP=$(wget -qO- icanhazip.com);
 apt install jq curl -y
 clear
+echo -e ""
+echo -e "\033[96;1m============================\033[0m"
+echo -e "\033[93;1m      INPUT SUNDOMAIN"
+echo -e "\033[96;1m============================\033[0m"
+echo -e "\033[91;1m Note. contoh Subdomain :\033[0m \033[93mbodoh22 \033[0m"
 echo -e " "
-echo -e "\033[93;1m==============================\033[0m"
-echo -e "\033[96;1m     INPUT YOUR DOMAIN "
-echo -e "\033[93;1m==============================\033[0m"
-echo -e " "
-read -p "INPUT YOUR SUBDOMAIN contoh( lwnwwjwk ):  " domen
-DOMAIN=mypremium.biz.id
+read -p "SUBDOMAIN :  " domen
+echo -e ""
+#DOMAIN=mypremium.biz.id
+DOMAIN=zvx.my.id
 sub=${domen}
+dns=${sub}.zvx.my.id
 #(</dev/urandom tr -dc a-z0-9 | head -c5)
-dns=${sub}.mypremium.biz.id
-CF_ID=andyyuda41@gmail.com
-CF_KEY=0d626234700bad388d6d07b49c42901445d1c
+#dns=${sub}.mypremium.biz.id
+#CF_ID=andyyuda41@gmail.com
+#CF_KEY=0d626234700bad388d6d07b49c42901445d1c
+dns=${sub}.zvx.my.id
+CF_KEY=e03f30d53ad7ec2ab54327baa5e2da5ab44f0
+CF_ID=mezzqueen293@gmail.com
 set -euo pipefail
 IP=$(wget -qO- icanhazip.com);
 echo "Updating DNS for ${dns}..."
